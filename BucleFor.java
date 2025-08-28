8cd0ddeabc2f74849cb1d5e60e73e1de8ff790d5
abrahamNunez7 <abraham.nunez@fisica.uaz.edu.mx>
2025-08-28 11:41:37 -0600

    Se realizó el primer programa de la serie de ejercicios de bucles, empezamos con la introducción al bucle 'for'

---
+++ BucleFor.java
@@ -0,0 +1,10 @@
+public class BucleFor {
+	//se hace un programa para practicar el bucle 'for'
+
+	//iniciamos el método principal
+	public static void main(String[] args) {
+	for (int i = 1 ; i <= 5 ; i++ ) {
+		System.out.println("Iteración número: " + i);
+	}
+	}
+}
